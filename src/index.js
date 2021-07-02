9e86f624460a06d0276f429734dc62c3a19756f7
SergeRomanenko13 <serhiiromanenko13@gmail.com>
2021-07-02 11:43:14 +0300

    1st commit

--- src/index.js
+++ src/index.js
@@ -18,33 +18,26 @@ const user = {
         },
     },
 };
+
 // console.log(user);
 
 function myDeepFreeze(obj) {
     Object.freeze(obj);
-    // console.log(obj, 'is frosen');
     for (const key in obj) {
         if (typeof obj[key] === 'object') {
             return myDeepFreeze(obj[key]);
         }
-        // console.log(key);
-        // console.log(obj);
     }
-    // console.log(obj); ///  WTF???
-    return obj;
 }
 
 myDeepFreeze(user);
 
 user.name = 'OLOLO';
 delete user.name;
-
 user.car.model = 'AUDI';
 delete user.car.model;
-// console.log(user.car);
 
 // console.log(Object.isFrozen(user.car.country));
 // console.log(Object.isFrozen(user.car));
 // console.log(Object.isFrozen(user));
-
 // console.log(user);
